a33b38cd3305c72e4193cbc7fddf4010d565d0e2
SensiPhi <lailtoneletrohits@hotmail.com>
2020-02-01 12:42:21 -0300

    Email validation completed

---
+++ base-apparel-coming-soon-master/javascript/script.js
@@ -0,0 +1,24 @@
+const email = document.querySelector("#email");
+const button = document.querySelector(".button");
+const msg = document.querySelector(".msg");
+const error_img = document.querySelector(".error-img");
+
+function validateEmail() {
+    if(!email.checkValidity()) {
+        console.log("E-mail inválido");
+        email.classList.add("invalid");
+        msg.classList.add("msg-invalid");
+        error_img.classList.add("active");
+        msg.innerHTML = "Please provide a valid email";
+    } else {
+        email.classList.remove("invalid");
+        msg.classList.remove("msg-invalid");
+        msg.innerHTML = "Email sent!";
+        error_img.classList.remove("active");
+    }
+};
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    validateEmail();
+});
